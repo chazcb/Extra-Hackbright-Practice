74672e38045e198e3fc9ef1ce053017b158de182
Charles Covey-Brandt <chazcb@gmail.com>
2013-10-12 12:28:44 -0700

    Adding skills1 practice.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Practice Skills for Hackbright
+
+## Extension of the Hackbright Curriculum skills\*.py worksheets.
+
+These drills are intended to give an additional opportunity to firm up concepts covered in their respective Hackbright skills\*.py worksheets.
---
+++ skills1.py
@@ -0,0 +1,96 @@
+# Things you should be able to do.
+
+list_of_things = [
+    19,
+    'hello world',
+    'such trasure',
+    [1, 2, 3, 4],
+    5467.829,
+    123.90,
+    123,
+    ['doge doge', 23, ['doge', 5, 'much doge, so want', [90000], {}], 4],
+    {'key': 'value'},
+    'fortune',
+    'doge wow',
+    'http://knowyourmeme.com/memes/doge'
+    'so wow',
+    10,
+    999
+]
+
+
+# Write a function that takes a nested list and filters for only odd numbers.
+def all_odd(some_list):
+    return []
+
+print all_odd(list_of_things)
+
+
+# Write a function that takes a nested list and filters for only even numbers.
+def all_even(some_list):
+    return []
+
+print all_even(list_of_things)
+
+
+# Write a function that filters a nested list for strings smaller than 10 characters.
+def short_words(word_list):
+    return []
+
+print short_words(list_of_things)
+
+
+# Write a function that finds the smallest number in a nested list and returns it.
+def smallest(some_list):
+    return None
+
+print smallest(list_of_things)
+
+
+# Write a function that finds the largest number in a nested list and returns it.
+def largest(some_list):
+    return None
+
+print largest(list_of_things)
+
+
+# Write a function that finds all numbers in a nested list and returns those numbers divided by two.
+def halvesies(some_list):
+    return []
+
+print halvesies(list_of_things)
+
+
+# Write a function that finds all strings in a nested list and returns a list of their lengths.
+def word_lenths(word_list):
+    return []
+
+print word_lenths(list_of_things)
+
+
+# Write a function (using iteration) that sums all the numbers in a nested list.
+def sum_numbers(numbers):
+    return []
+
+print sum_numbers(list_of_things)
+
+
+# Write a function that multiplies all the numbers in a nested list together and returns one number.
+def mult_numbers(numbers):
+    return None
+
+print mult_numbers(list_of_things)
+
+
+# Write a function that joins all the strings in a nested list together and returns a single string.
+def join_strings(string_list):
+    return ""
+
+print join_strings(list_of_things)
+
+
+# Write a function that takes a nested list of integers and returns the average.
+def average(numbers):
+    return None
+
+print average(list_of_things)
